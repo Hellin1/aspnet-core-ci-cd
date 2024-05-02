39475719871150fdf527917432e016f0bfdac2f6
Halil Bulut <70853592+Hellin1@users.noreply.github.com>
2024-05-02 11:19:12 +0000

    added docker, docker compose and start.sh for ci/cd stuff

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_ci_cd.Controllers;
+
+[ApiController]
+[Route("/test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "techbuddy";
+    }
+
+}
